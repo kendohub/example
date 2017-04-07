9b3a38f1fa26d0c8de492aa5dd4eb405d799e410
k-endo <k-endo@ttc-net.co.jp>
2017-04-07 10:12:40 +0900

    geolocation sample

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -6,13 +6,52 @@ var channel = socket.channel("room:lobby", {});
 channel.join();
 
 $("form").submit(function(e) {
-    e.preventDefault();
-    channel.push("send_message", {message: $("#input-send-message").val()});
-    $("#input-send-message").val("");
+    send_message(e, $("#input-send-message").val())
 });
 
 channel.on("receive_message", function(dt) {
-    var div = $("<div></div>", {"class": "received-message"})
-        .text(dt.message);
-    $("#received-messages").prepend(div);
+    prepend_message(dt.message);
 });
+
+function send_message(e, message) {
+    e.preventDefault();
+    channel.push("send_message", {message: message});
+    $("#input-send-message").val("");
+}
+
+function prepend_message(message) {
+    var div = $("<div></div>", {"class": "received-message"})
+        .text(message);
+    $("#received-messages").prepend(div);
+}
+
+// ****
+// @see https://developer.mozilla.org/ja/docs/WebAPI/Using_geolocation
+var watchID;
+// ****
+$("#start").on("click", function(e) {
+  watchID = geoStart();
+});
+
+$("#stop").on("click", function(e) {
+  watchID = geoStop();
+});
+
+function geoStart() {
+  if (!navigator.geolocation){
+    return null;
+  }
+
+  return navigator.geolocation.watchPosition(function(position) {
+    var latitude  = position.coords.latitude;
+    var longitude = position.coords.longitude;
+    channel.push("send_message", {message: latitude + "," + longitude});
+  }, function() {
+    prepend_message("error");
+  });
+};
+
+function geoStop() {
+  navigator.geolocation.clearWatch(watchID);
+  return null;
+}
